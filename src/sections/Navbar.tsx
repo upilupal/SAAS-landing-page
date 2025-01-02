f13baa37add6c801fadcf365174ea55fa29246a1
upilupal <naufalhanif891@gmail.com>
2025-01-02 17:42:34 +0700

    feat: navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import logoImage from "@/assets/images/logo.svg";
+import { Button } from "@/components/ui/button";
+
 const navLinks = [
     { label: "Home", href: "#" },
     { label: "Features", href: "#features" },
@@ -6,5 +10,53 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <section className="py-4 lg:py-8">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
+                    <div>
+                        <Image
+                            src={logoImage}
+                            alt="Layers Logo"
+                            className="h-9 md:h-auto w-auto"
+                        />
+                    </div>
+                    <div className="lg:flex justify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map((link) => (
+                                <a href={link.href} key={link.label}>
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="flex justify-end gap-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="feather feather-menu md:hidden"
+                        >
+                            <line x1="3" y1="12" x2="21" y2="12"></line>
+                            <line x1="3" y1="6" x2="21" y2="6"></line>
+                            <line x1="3" y1="18" x2="21" y2="18"></line>
+                        </svg>
+
+                        <Button className="hidden h-12 px-6 " variant="outline">
+                            Log In
+                        </Button>
+                        <Button className="hidden h-12 px-6" variant="default">
+                            Sign Up
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
