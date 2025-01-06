87baa7fbcc312392a9740cd07f74f77d466118c9
upilupal <naufalhanif891@gmail.com>
2025-01-06 14:47:58 +0700

    feat: footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,5 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +7,22 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <div>
+                        <Image src={logoImage} alt="layers logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a href={link.href} className="text-white/50 text-sm">{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
